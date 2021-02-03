1caf84e00cde5a90d3682253ee6dbe0c46d80d1d
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-02-03 17:22:27 +0900

    added User

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,15 +2,21 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import permissions
 from django.http import Http404
+from django.contrib.auth.models import User
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
 
 
 class SnippetList(APIView):
     """
     List all snippets, or create a new snippet
     """
+
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -22,6 +28,9 @@ class SnippetList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(APIView):
@@ -53,6 +62,13 @@ class SnippetDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 
